a5c7a46ac9d90f9fdfc52c80d537032fbbb9e7aa
liangxiaoxin <651589762@qq.com>
2018-05-14 17:03:05 +0800

    查询电影列表接口

--- src/routers/movie.js
+++ src/routers/movie.js
@@ -8,5 +8,18 @@ router.get('/movie/:doubanId',async (ctx, next) =>  {
   let movie = await Movie.findOne({doubanId})
   ctx.body = movie
 });
+router.get('/list',async (ctx, next) =>  {
+  let doubanId = Number(ctx.params.doubanId)
+  console.log('doubanId',doubanId);
+  let movieList = await Movie.find().limit(10)
+  let copyList = movieList.map(({title,doubanId,rate,poster})=>{
+    return {title,doubanId,rate,poster}
+  })
+  ctx.body = {
+    msg:'ok',
+    status:200,
+    data:copyList
+  }
+});
 
 module.exports = router;
